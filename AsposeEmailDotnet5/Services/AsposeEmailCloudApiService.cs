32c665b4e615b6c9f2b42855fdd06f4d2125137f
ShakeelFaiz <shakeel.faiz@aspose.com>
2021-06-24 14:42:59 +0500

    Session Extensions removed

--- AsposeEmailDotnet5/Services/AsposeEmailCloudApiService.cs
+++ AsposeEmailDotnet5/Services/AsposeEmailCloudApiService.cs
@@ -1,31 +1,14 @@
 ﻿using Aspose.Email.Cloud.Sdk.Api;
 using Aspose.Email.Cloud.Sdk.Model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using System.IO;
-using System.Text.Json;
 
 namespace AsposeEmailDotnet5.Services
 {
-    public static class SessionExtensions
-    {
-        public static void Set<T>(this ISession session, string key, T value)
-        {
-            session.SetString(key, JsonSerializer.Serialize(value));
-        }
-
-        public static T Get<T>(this ISession session, string key)
-        {
-            var value = session.GetString(key);
-            return value == null ? default : JsonSerializer.Deserialize<T>(value);
-        }
-    }
-
     public interface IAsposeEmailCloudApiService
     {
         Stream Convert(Stream file, string fromFormat, string toFormat);
     }
-
 
     public class AsposeEmailCloudApiService : IAsposeEmailCloudApiService
     {
